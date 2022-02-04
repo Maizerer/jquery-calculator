c59945eca28497c54bab4d299087008ab6086096
Rustem <ryuzikc@gmail.com>
2022-02-04 17:33:08 +0300

    Made calc interface

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,54 @@
-document.addEventListener('DOMContentLoaded',()=>{
- 
+$(document).ready(function(){
+
+    const input = $('#input')
+
+    $('#clear').click(function() {
+        input.val('')
+    });
+
+    $('#divider').click(function() {
+        let inputValue = input.val()
+        input.val(inputValue + ',')
+    });
+
+    $('#one').click(function() {
+        let inputValue = input.val()
+        input.val(inputValue + '1')
+    });
+    $('#two').click(function() {
+        let inputValue = input.val()
+        input.val(inputValue + '2')
+    });
+    $('#three').click(function() {
+        let inputValue = input.val()
+        input.val(inputValue + '3')
+    });
+    $('#four').click(function() {
+        let inputValue = input.val()
+        input.val(inputValue + '4')
+    });
+    $('#five').click(function() {
+        let inputValue = input.val()
+        input.val(inputValue + '5')
+    });
+    $('#six').click(function() {
+        let inputValue = input.val()
+        input.val(inputValue + '6')
+    });
+    $('#seven').click(function() {
+        let inputValue = input.val()
+        input.val(inputValue + '7')
+    });
+    $('#eight').click(function() {
+        let inputValue = input.val()
+        input.val(inputValue + '8')
+    });
+    $('#nine').click(function() {
+        let inputValue = input.val()
+        input.val(inputValue + '9')
+    });
+    $('#zero').click(function() {
+        let inputValue = input.val()
+        input.val(inputValue + '0')
+    });
 })
